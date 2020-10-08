f6ff0f3cc048c2f3428816154f7f72f4bb4cff7a
Senchoz <arsenio.smirnov@gmail.com>
2020-10-08 16:29:16 +0300

    Added settings.py with Settings() class to store game settigns separately

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Class for storing all the settings for the game"""
+
+    def __init__(self):
+        """Initialize game settings"""
+        # Screen parameters
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
